059a48f91ed6ec68f12dd05a45db4c464413d773
SungPo Chang <p03082002@gmail.com>
2021-10-10 23:16:34 +0800

    test

--- bothandlerEngMark1.py
+++ bothandlerEngMark1.py
@@ -384,7 +384,7 @@ def talkuser(messaging_event):
                     client.send_text_message(recipient_id,"I have recognized some text fragments:\n"+addr)
                     client.send_text_message(recipient_id,"then try to combine....")
                     georesult = requests.get(url = "http://api.opencube.tw/location/address", params = {'keyword':addr, 'key':google_API_Key}) 
-                    geojson = georesult.json()
+                    # geojson = georesult.json()
                     # if geojson['status'] == 200:  # if the api return OK status
                     #     client.send_text_message(recipient_id, geojson['data']['full_address'])
                     #     btn_tmp = [ActionButton(ButtonType.POSTBACK,"address_correct", payload = geojson['data']['full_address']), ActionButton(ButtonType.POSTBACK, "Add Detail", payload = geojson['data']['full_address'])]
